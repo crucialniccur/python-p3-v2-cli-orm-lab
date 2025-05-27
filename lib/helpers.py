91aa8221474701e10ef5524c86c9609c0fc994af
Mati Joseph <crucialniccur@gmail.com>
2025-05-27 14:55:42 +0300

    define list_employees

--- lib/helpers.py
+++ lib/helpers.py
@@ -68,7 +68,8 @@ def delete_department():
 # You'll implement the employee functions in the lab
 
 def list_employees():
-    pass
+    employees = Employee.get_all()
+    return [print(employee) for employee in employees]
 
 
 def find_employee_by_name():
